2704f25ed2477d09e001b5d406d6a20323d98bff
bai <17605229265@163.com>
2025-03-21 21:14:29 +0800

    test

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,8 @@
+#multiply three numbers
+#author: Zou Yeqing
+num1=int(input("please enter a number:" ))
+num2=int(input("please enter a number:" ))
+num3=int(input("please enter a number:" ))
+result = num1*num2*num3
+
+print(result)
